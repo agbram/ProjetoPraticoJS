b4db4892a1ba2833048c42b5894325bfbb236aee
andresilva05 <andresilva9657@gmail.com>
2025-11-14 21:51:01 -0300

    feat: adiciona funcionalidade de exibição de filmes e ajusta links de navegação

--- Js/filmes.js
+++ Js/filmes.js
@@ -1,4 +1,3 @@
-
 // Função para buscar dados da SWAPI
 async function buscarDados(endpoint) {
   const url = `https://swapi.dev/api/${endpoint}`;
@@ -18,3 +17,44 @@ async function buscarDados(endpoint) {
     return [];
   }
 }
+
+const filmes = await buscarDados("films");
+
+function adicionaCards(listaFilmes) {
+  const container = document.getElementById("listaFilmes");
+
+  listaFilmes.forEach((filmes) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardDiv.style.width = "18rem";
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const titulo = document.createElement("p");
+    titulo.textContent = `Título: ${filmes.title}`;
+    const diretor = document.createElement("p");
+    diretor.textContent = `Diretor: ${filmes.director}`;
+
+    cardDiv.appendChild(cardBody);
+    cardBody.appendChild(titulo);
+    cardBody.appendChild(diretor);
+
+    container.appendChild(cardDiv);
+  });
+}
+
+adicionaCards(filmes);
+
+// Selecionando o input pelo seu ID
+let meuInput = document.getElementById("buscaFilme");
+const botao = document.getElementById("btnBuscar");
+
+botao.addEventListener("click", function () {
+  // Pega o valor do input e o exibe no console
+  const valorInput = meuInput.value;
+
+  meuInput.value = "";
+
+  console.log(valorInput);
+});
